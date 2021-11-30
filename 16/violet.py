62ba5adbdf3dbbb4d3a99adcadf5d58c3fa4b733
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-11-30 23:33:11 +0800

    remove key

--- 16/violet.py
+++ 16/violet.py
@@ -39,7 +39,8 @@ def main():
     map_options = GMapOptions(lat=0, lng=0, zoom=3)
     plot = GMapPlot(x_range=Range1d(), y_range=Range1d(), map_options=map_options)
     plot.title.text = "Example Plot"
-    plot.api_key = environ["API"]
+    # plot.api_key = environ["API"]
+    plot.api_key = ""
     source = ColumnDataSource(data=dict(lat=lats, lon=longs))
     circle = Circle(
         x="lon", y="lat", size=10, fill_color="red", fill_alpha=0.6, line_color=None
